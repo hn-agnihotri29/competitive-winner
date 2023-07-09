8825de1791df82d9a10b19accd6ad11f898486db
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2023-07-09 21:52:10 +0530

    Create 78.Subsets.cpp

---
+++ Leetcode/78.Subsets.cpp
@@ -0,0 +1,16 @@
+vector<vector<int>> powerset;
+    vector<int> subset; 
+    vector<vector<int>> subsets(vector<int>& nums) {
+        backtrack(nums, 0);
+        return powerset;
+    }
+    void backtrack(vector<int>& nums,int start) {
+        powerset.push_back(subset);
+
+        for(int i = start; i < nums.size(); i++) {
+            subset.push_back(nums[i]);
+            backtrack(nums, i  + 1);
+
+            subset.pop_back();
+        }
+}
